f816f11c8cc4b803a99eb51e5ea2058f8bc88014
Wojciech Plokarz <wojciech.plokarz@op.pl>
2023-02-11 20:07:42 +0100

    Tests_started

--- Question_manager/tests.py
+++ Question_manager/tests.py
@@ -1,3 +1,45 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+import pytest
+from django.shortcuts import reverse, redirect
+
+from Test_manager.models import Questions
+from User_manager.models import User, League
+
 
 # Create your tests here.
+@pytest.fixture
+def client():
+    return Client()
+
+
+@pytest.mark.django_db
+def test_QuestionTable_view(client):
+    url = reverse('all_questions')
+    response = client.get(url)
+    assert response.status_code == 302
+    assert response.url == '/no_permission/'
+
+
+@pytest.mark.django_db
+def test_CreateQuestion_view(client):
+    url = reverse('create_question')
+    response = client.get(url)
+    assert response.status_code == 302
+    assert response.url == '/no_permission/'
+
+
+@pytest.mark.django_db
+def test_EditQuestion_view(client):
+    league = League.objects.create(which_league="test")
+    user = User.objects.create_user(
+        first_name="first_name",
+        last_name="last_name",
+        username="test@test.pl",
+        password='password', )
+    question = Questions.objects.create(add_question="test", question_possible_answer='test',
+                                        question_correct_answer='test',
+                                        for_league=league, added_by=user)
+    url = reverse('edit_question', question.slug)
+    response = client.get(url)
+    assert response.status_code == 302
+    assert response.url == '/no_permission/'
--- User_manager/tests.py
+++ User_manager/tests.py
@@ -1,3 +1,21 @@
-from django.test import TestCase
-
+from django.test import TestCase, Client
+from User_manager.models import User
+from django.shortcuts import reverse, redirect
+import pytest
 # Create your tests here.
+@pytest.fixture
+def client():
+    return Client()
+
+@pytest.mark.django_db
+def test_login_view(client):
+    User.objects.create_user(
+        first_name="first_name",
+        last_name="last_name",
+        username="test@test.pl",
+        password='password',)
+    url = reverse('login')
+    response = client.post(url, {'username': 'test@test.pl', 'password': 'password'})
+    assert response.status_code == 302
+    assert response.url == '/'
+    assert client.session.get('_auth_user_id') is not None
