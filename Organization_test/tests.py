ca412c01fde93287e84ccaa453eac41294520304
Wojciech Plokarz <wojciech.plokarz@op.pl>
2023-02-14 19:05:12 +0100

    tests_in_progress

--- Organization_test/tests.py
+++ Organization_test/tests.py
@@ -45,3 +45,29 @@ def test_organize_test(client, user_in_admin_group):
     client.post(url, {'test_for_league': league, 'test_number': test_number, "date_time": date_obj})
     # assert OrganiseTest.objects.all().count() == 1
 
+
+@pytest.mark.django_db
+def test_for_user_list(client, user_in_admin_group):
+    url = reverse('test_solving')
+    response = client.get(url)
+    assert response.status_code == 302
+    assert response.url == '/login/?next=/test_solving/'
+
+@pytest.mark.django_db
+def test_specific_test_to_solve(client, user_in_admin_group):
+    url = reverse('login')
+    client.post(url, {'email': 'test@test.pl', 'password': 'password'})
+    league = League.objects.create(which_league="testowa liga")
+    test_in_alltest = AllTest.objects.create(test_name="test", date="2023-05-05", for_league=league)
+    date_time_to_add = '2023-02-28'
+    date_obj = datetime.strptime(date_time_to_add, "%Y-%m-%d").date()
+    OrganiseTest.objects.create(test_for_league=league, test_number=test_in_alltest, date_time=date_obj)
+    organise = OrganiseTest.objects.get(id=1)
+    url = reverse('specific_test_solve', kwargs={'slug': organise.slug})
+    response = client.get(url)
+    assert OrganiseTest.objects.all().count() == 1
+    assert response.status_code == 200
+
+@pytest.mark.django_db
+def test_SpecificTestSolved(client, user_in_admin_group):
+    pass
