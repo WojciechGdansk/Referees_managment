c54d917c30996996783feca79ee6db1dbd6aa6f3
Wojciech Plokarz <wojciech.plokarz@op.pl>
2023-02-16 10:16:41 +0100

    Final_project_test_fixed

--- Question_manager/tests.py
+++ Question_manager/tests.py
@@ -64,14 +64,14 @@ def test_CreateQuestion_view(client, user_in_admin_group):
     assert Questions.objects.all().count() == 0
     AllPossibleAnswers.objects.create(all_kind_answers="Test2")
     league = League.objects.create(which_league="testowa liga5")
+    league_id = League.objects.first().id
     assert League.objects.all().count() == 1
     assert AllPossibleAnswers.objects.all().count() == 1
-    client.post(url, data={"add_question": "testowe pytanie",
+    r = client.post(url, data={"add_question": "testowe pytanie555",
                            'question_possible_answer': ['1'],
                            'question_correct_answer': ['1'],
-                           'for_league': ['1']
+                           'for_league': [league_id]
                            })
-
     assert Questions.objects.count() == 1
 
 @pytest.mark.django_db
@@ -83,13 +83,15 @@ def test_EditQuestion_view(client, user_in_admin_group):
     assert response.status_code == 200
     assert Questions.objects.all().count() == 0
     AllPossibleAnswers.objects.create(all_kind_answers="Test10")
+    answers_id = AllPossibleAnswers.objects.last().id
     league = League.objects.create(which_league="testowa liga10")
+    league_id = League.objects.first().id
     assert League.objects.all().count() == 1
     assert AllPossibleAnswers.objects.all().count() == 1
     client.post(url, data={"add_question": "testowe pytanie99",
-                           'question_possible_answer': ['1'],
-                           'question_correct_answer': ['1'],
-                           'for_league': ['1']
+                           'question_possible_answer': [answers_id],
+                           'question_correct_answer': [answers_id],
+                           'for_league': [league_id]
                            })
 
     assert Questions.objects.count() == 1
@@ -98,15 +100,15 @@ def test_EditQuestion_view(client, user_in_admin_group):
     response = client.get(url)
     assert response.status_code == 200
     client.post(url, data={"add_question": "testowe pytanie2",
-                           'question_possible_answer': ['1'],
-                           'question_correct_answer': ['1'],
-                           'for_league': ['1']
+                           'question_possible_answer': [answers_id],
+                           'question_correct_answer': [answers_id],
+                           'for_league': [league_id]
                            })
     assert Questions.objects.last().add_question == "testowe pytanie2"
 
 
 @pytest.mark.django_db
-def test_DeleteQuesiton(client):
+def test_DeleteQuesiton(client, user_in_admin_group):
     user = User.objects.last()
     client.force_login(user)
     url = reverse("create_question")
@@ -114,19 +116,21 @@ def test_DeleteQuesiton(client):
     assert response.status_code == 200
     assert Questions.objects.all().count() == 0
     AllPossibleAnswers.objects.create(all_kind_answers="Test456789")
+    answers_id = AllPossibleAnswers.objects.last().id
     league = League.objects.create(which_league="testowa liga12345")
+    league_id = League.objects.first().id
     assert League.objects.all().count() == 1
     assert AllPossibleAnswers.objects.all().count() == 1
     client.post(url, data={"add_question": "testowe pytanie",
-                           'question_possible_answer': ['1'],
-                           'question_correct_answer': ['1'],
-                           'for_league': ['1']
+                           'question_possible_answer': [answers_id ],
+                           'question_correct_answer': [answers_id ],
+                           'for_league': [league_id]
                            })
 
     assert Questions.objects.count() == 1
     question = Questions.objects.last()
     url = reverse("delete_question", kwargs={"slug": question.slug})
     response = client.get(url)
-    assert response.status_code == 200
+    assert response.status_code == 302
     assert Questions.objects.count() == 0
 
